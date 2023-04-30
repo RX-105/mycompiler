16302be5f1c17bf1b731720937b12b87d02576d6
n0sense <1436648676@qq.com>
2023-04-30 16:25:44 +0800

    测试移除注释的方法

--- src/test/java/io/n0sense/mycompiler/util/SyntaxUtilTest.java
+++ src/test/java/io/n0sense/mycompiler/util/SyntaxUtilTest.java
@@ -3,10 +3,12 @@ package io.n0sense.mycompiler.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class SyntaxUtilTest {
     @Test
     void testBracketMatcher() {
-        String testFile = "C:\\Users\\kazuha\\projects\\java-projects\\mycompiler\\source.c";
+        File testFile = new File("C:\\Users\\kazuha\\projects\\java-projects\\mycompiler\\source.c");
         boolean result = SyntaxUtil.isBracketMatched(testFile);
         System.out.println("result = " + result);
         Assertions.assertTrue(result);
@@ -18,5 +20,12 @@ public class SyntaxUtilTest {
         String line2 = "int main(int argv, char* argc)";
         System.out.println(SyntaxUtil.split(line1));
         System.out.println(SyntaxUtil.split(line2));
+    }
+
+    @Test
+    void testRemoveComments() {
+        File testFile = new File("C:\\Users\\kazuha\\projects\\java-projects\\mycompiler\\source.c");
+        boolean res = SyntaxUtil.removeComments(testFile);
+        Assertions.assertTrue(res);
     }
 }
