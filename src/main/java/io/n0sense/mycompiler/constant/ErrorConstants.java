036794ec0894e71b35e8260937828224001a3b7e
n0sense <1436648676@qq.com>
2023-04-30 18:06:51 +0800

    bundle改用配置文件提供的locale

--- src/main/java/io/n0sense/mycompiler/constant/ErrorConstants.java
+++ src/main/java/io/n0sense/mycompiler/constant/ErrorConstants.java
@@ -1,11 +1,13 @@
 package io.n0sense.mycompiler.constant;
 
-import java.util.Locale;
+import io.n0sense.mycompiler.util.I18nUtil;
+
 import java.util.ResourceBundle;
 
 public interface ErrorConstants {
-    ResourceBundle bundle = ResourceBundle.getBundle("bundle", Locale.getDefault());
+    ResourceBundle bundle = ResourceBundle.getBundle("bundle", I18nUtil.getLocale());
     String error = bundle.getString("execution_error_header");
+    String noArgSpecified = bundle.getString("no_arg_specified");
     String invalidFile = bundle.getString("invalid_file");
     String cantWriteFile = bundle.getString("cant_write_file");
     String invalidNumber = bundle.getString("invalid_number");
