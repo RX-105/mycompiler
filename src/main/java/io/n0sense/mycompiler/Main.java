7f1fe9ca4af5a5ac9cbcebecbdc50f841f20eff6
n0sense <1436648676@qq.com>
2023-04-30 18:08:01 +0800

    移除多余代码，添加无参数错误提示

--- src/main/java/io/n0sense/mycompiler/Main.java
+++ src/main/java/io/n0sense/mycompiler/Main.java
@@ -2,19 +2,15 @@ package io.n0sense.mycompiler;
 
 import io.n0sense.mycompiler.commands.AnalyzeCommand;
 import io.n0sense.mycompiler.commands.HelpCommand;
+import io.n0sense.mycompiler.constant.ErrorConstants;
+import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) {
-        Locale locale_en_US = new Locale("en", "US");
-        ResourceBundle bundle = ResourceBundle.getBundle("bundle", locale_en_US);
-        System.out.println(bundle.getString("general_help"));
         AnsiConsole.systemInstall();
         if (args.length == 0)
-            new HelpCommand().run(args, -1);
+            System.err.println(Ansi.ansi().fgRed().a(ErrorConstants.noArgSpecified).reset());
         else {
             if (args[0].equals("-h") || args[0].equals("--help")) {
                 new HelpCommand().run(args, 1);
